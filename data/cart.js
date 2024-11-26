3431f3e2f1c09ee544f74a166f95c6ecd59c708c
AmanCS <amancodecs17@gmail.com>
2024-11-26 16:36:57 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
-export let cart= 
+export let cart=  JSON.parse(localStorage.getItem('cart'));
 
-
+if(!cart) {
 [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
@@ -9,6 +9,7 @@ export let cart=
     quantity: 1
 }];
 
+}
  function saveToStorage(){
   localStorage.setItem('cart', JSON.stringify(cart));
  } 
