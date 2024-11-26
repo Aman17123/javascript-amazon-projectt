68a451518ad429ab78117bab840ba38ab6704b04
AmanCS <amancodecs17@gmail.com>
2024-11-26 16:29:25 +0530

    reomve product from cart ehrn press delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart= [{
+export let cart= 
+
+
+[{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
   }, {
@@ -6,6 +9,9 @@ export let cart= [{
     quantity: 1
 }];
 
+ function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+ } 
 export function addtoCart(productId){
     let matchingItem; // cart 
 
@@ -24,6 +30,8 @@ export function addtoCart(productId){
         quantity: 1
       }); 
     }
+
+    saveToStorage();
   }
 
   export function removefromCart(productId){
@@ -38,4 +46,6 @@ export function addtoCart(productId){
     });
 
     cart = newCart;
+
+    saveToStorage();
   }
